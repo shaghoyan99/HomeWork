701ff34fe751c3ccbdb2eb227c27f36816b48b70
Artash Shaghoyan <shagoyan99@gmail.com>
2025-09-14 00:17:17 +0400

    Add home work 2 update

--- src/Work2.java
+++ src/Work2.java
@@ -3,17 +3,18 @@ public class Work2 {
 
     public static void main(String[] args) {
 
+        int n = 5;
 
         System.out.println("Խնդիր 1");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
         System.out.println("Խնդիր 2");
-        for (int i = 5; i > 0; i--) {
+        for (int i = n; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
@@ -21,41 +22,42 @@ public class Work2 {
         }
 
         System.out.println("Խնդիր 3");
-        for (int i = 5; i > 0; i--) {
-            for (int k = 0; k < i; k++) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 5; j >= i; j--) {
+            for (int j = n; j > i - 1; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
         System.out.println("Խնդիր 4");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 5; k > i; k--) {
+            for (int j = n; j > i; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
         System.out.println("Խնդիր 5");
-        for (int i = 5; i > 0; i--) {
-            for (int k = 0; k < i; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int s = 0; s < n - 1 - i; s++) {
                 System.out.print(" ");
             }
-            for (int j = 5; j > i; j--) {
-                System.out.print("* ");
+            for (int յ = 0; յ < i + 1; յ++) {
+                System.out.print(" *");
             }
             System.out.println();
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i; j++) {
+        for (int i = n - 2; i >= 0; i--) {
+            for (int յ = 0; յ < n - 1 - i; յ++) {
                 System.out.print(" ");
             }
-            for (int k = 5; k > i; k--) {
-                System.out.print("* ");
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" *");
             }
             System.out.println();
         }
