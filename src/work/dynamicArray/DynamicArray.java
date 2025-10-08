09d27ef06f1e609f16ffc0862e42bbd39617490d
Artash Shaghoyan <shagoyan99@gmail.com>
2025-10-08 09:50:54 +0400

    add homework DynamicArray

--- src/work/dynamicArray/DynamicArray.java
+++ src/work/dynamicArray/DynamicArray.java
@@ -15,10 +15,39 @@ public class DynamicArray {
     }
 
     public void add(int value) {
-        if (size == array.length - 1) {
+        if (size == array.length) {
             extend();
         }
         array[size++] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            System.out.println(index + " ինդեքսով էլեմենտ չկա մասիվում․ ");
+        } else {
+            array[index] = value;
+            System.out.println("Տվյալ " + index + "-ի արժեքը փոփոխվեց․ ");
+        }
+    }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > size) {
+            System.out.println(index + " ինդեքսով էլեմենտ չկա մասիվում․ ");
+        } else {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
+        }
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println(index + " ինդեքսով էլեմենտ չկա մասիվում․ ");
+        } else {
+            array[index] = value;
+        }
+
     }
 
     private void extend() {
@@ -34,6 +63,34 @@ public class DynamicArray {
             return -1;
         }
         return array[index];
+    }
+
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        int indexByValue = -1;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                count++;
+                if (count > 1) {
+                    return indexByValue;
+                }
+                indexByValue = i;
+            }
+        }
+        if (indexByValue < 0 || indexByValue > size) {
+            return -1;
+        } else {
+            return indexByValue;
+        }
     }
 
     public void print() {
--- src/work/dynamicArray/DynamicArrayDemo.java
+++ src/work/dynamicArray/DynamicArrayDemo.java
@@ -3,12 +3,25 @@ package work.dynamicArray;
 public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray dynamicArray = new DynamicArray();
-        for (int i = 1; i < 15; i++) {
-            dynamicArray.add(i);
-        }
+        boolean resultValue;
+        int indexByValue;
+        dynamicArray.add(5);
+        dynamicArray.add(9);
+        dynamicArray.add(8);
+        dynamicArray.add(78);
+        dynamicArray.add(7);
+        dynamicArray.add(7);
         dynamicArray.print();
-        System.out.println(dynamicArray.getByIndex(7));
-        System.out.println(dynamicArray.getByIndex(15));
+        dynamicArray.deleteByIndex(0);
+        dynamicArray.print();
+        dynamicArray.set(1,1);
+        System.out.println(dynamicArray.getByIndex(4));
+        resultValue = dynamicArray.exists(7);
+        System.out.println(resultValue);
+
+        indexByValue = dynamicArray.getIndexByValue(7);
+        System.out.println(indexByValue);
+        dynamicArray.print();
 
 
     }
